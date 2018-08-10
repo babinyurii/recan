c3e8c1fd1d74f9b728a07a2a0b9ebad5951214b1
Yuriy Babin <babin.yurii@gmail.com>
2018-08-10 17:16:03 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,14 +5,21 @@ Created on Thu Aug  2 12:58:48 2018
 @author: babin
 """
 
-from setuptools import setup
+import setuptools
 
-setup(name='recan',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='recan',
       version='0.1',
-      description='recombination analysis tool',
-      url='',
       author='Yuriy Babin',
       author_email='babin.yurii@gmail.com',
-      license='',
-      packages=['recan'],
-      zip_safe=False)
+      description='recombination analysis tool',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/babinyurii/RECAN',
+      packages=setuptools.find_packages(),
+      classifiers=("Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",),)
+    
