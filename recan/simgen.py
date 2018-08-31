a1389a0f5e14d8f5c4f1261196eef9d7e775671a
Yuriy Babin <babin.yurii@gmail.com>
2018-08-31 13:01:20 +0300

    Add files via upload

--- recan/simgen.py
+++ recan/simgen.py
@@ -1,157 +1,177 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jul 31 17:59:26 2018
-@author: babin
-"""
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 import pandas as pd
 import plotly.graph_objs as go
-from Bio import AlignIO
+from Bio.Align import MultipleSeqAlignment
+from Bio.SeqRecord import SeqRecord
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 init_notebook_mode(connected=True)
 
 
-def _pdistance(seq1, seq2):
-    """calculates pairwise distance between two sequences"""
-    p = 0
-    pairs = []
-    for x in zip(seq1, seq2):
-        if '-' not in x:
-            pairs.append(x)
-    for (x, y) in pairs:
-        if x != y:
-            p += 1
-    length = len(pairs)
-    #assert length > 0, "AssertionError: perhaps your alignment contains only or too many gaps"
-    try:
-        dist = float(1 - p / length)  # '1 - p' to take plot 'upside down'
-        return dist
-    except ZeroDivisionError as e:
-        print(e, ": perhaps your alignment contains only gaps")
+class Simgen(MultipleSeqAlignment):
+      
+    def __init__(self, path):
+        """initializing Simgen"""
+        from Bio import AlignIO
+        recs_prepared = (x for x in AlignIO.read(path, "fasta")) # it works without it, but i believe it's formally right
+        super(Simgen, self).__init__(recs_prepared)  # you need explicitly call the __init__ of the upperclass
+        
+        self._distance = {}  # empty, until call of the simgen function
+        self._align = None  # current slice or whole MultipleSeqRecord for plotting
+        self._ticks = None  # ticks for plot
+        
+    
+    def _draw_simplot(self):
+        """draws similarity plot"""
+
+        data = []
+        for key in self._distance.keys():
+            trace = go.Scatter(y=self._distance[key], x=self._ticks, name=key)
+            data.append(trace)
+
+        layout = go.Layout(
+            xaxis=dict(
+                title="nucleotide position"),
+            yaxis=dict(
+                title="sequence identity"),
+            legend=dict(x=-0.1, y=1.5, orientation="h"))
+            #legend=dict(x=-0.1, y=1.5))
+
+        fig = go.Figure(data=data, layout=layout)
+        iplot(fig)
    
-        
- 
-def _draw_simplot(distance_data, tick_container):
-    """draws similarity plot"""
+    def _get_x_labels(self, left_border, right_border, shift):
+        """creates tick labels"""
 
-    data = []
-    for key in distance_data.keys():
-        trace = go.Scatter(y=distance_data[key], x=tick_container, name=key)
-        data.append(trace)
+        tick_container = []      
+        tick_container.append(left_border)
 
-    layout = go.Layout(
-        title="similarity plot",
-        xaxis=dict(
-            title="nucleotide position"),
-        yaxis=dict(
-            title="sequence identity"),
-        legend=dict(x=-0.1, y=1.5))
+        while tick_container[-1] < right_border:
+            tick_container.append(tick_container[-1] + shift)
+            if tick_container[-1] > right_border:
+                tick_container[-1] = right_border
 
-    fig = go.Figure(data=data, layout=layout)
-    iplot(fig)
-
-
-
-def _get_x_labels(left_border, right_border, shift):
-    """creates tick labels"""
+        self._ticks = tick_container
+     
     
-    tick_container = []      
-    tick_container.append(left_border)
+    def _move_window(self, window, pot_rec, shift):
+        """moves window"""
+        distance_data = {}
+        parents = list(range(0, len(self._align)))
+        parents.remove(pot_rec)
+        align_length = len(self._align[0, :])
+
+        for par in parents:
+            dist_container = []
+            start = 0
+            finish = shift
+
+            while start < align_length:
+                seq1 = self._align[pot_rec, start:finish].seq # here is a potential recombinant sequence slice
+                seq2 = self._align[par, start:finish].seq  # here's a parent's slice
+                dist_container.append(self._pdistance(seq1, seq2)) #calculate pdistance, append to container
+                start += shift
+                finish = start + window
+
+            distance_data[self._align[par].id] = dist_container
+
+        self._distance = distance_data  # do i really should return? it's better to get access just right
     
-    while tick_container[-1] < right_border:
-        tick_container.append(tick_container[-1] + shift)
-        if tick_container[-1] > right_border:
-            tick_container[-1] = right_border
-            
-    return tick_container
 
 
-def _move_window(align, window, pot_rec, shift):
-    """moves window"""
-    distance_data = {}
-    parents = list(range(0, len(align)))
-    parents.remove(pot_rec)
-    align_length = len(align[0, :])
+    def _pdistance(self, seq1, seq2):
+        """calculates pairwise distance between two sequences"""
+        p = 0
+        pairs = []
+        for x in zip(seq1, seq2):
+            if '-' not in x:
+                pairs.append(x)
+        for (x, y) in pairs:
+            if x != y:
+                p += 1
+        length = len(pairs)
+        #assert length > 0, "AssertionError: perhaps your alignment contains only or too many gaps"
+        try:
+            dist = float(1 - p / length)  # '1 - p' to take plot 'upside down'
+            return dist
+        except ZeroDivisionError as e:
+            print(e, ": perhaps your alignment contains only gaps")
+
     
-    for par in parents:
-        dist_container = []
-        start = 0
-        finish = shift
+    def simgen(self, pot_rec, window=500, shift=250, region=False):
+        """slices the alignment, collects the distance data
 
-        while start < align_length:
-            seq1 = align[pot_rec, start:finish].seq # here is a potential recombinant sequence slice
-            seq2 = align[par, start:finish].seq  # here's a parent's slice
-            dist_container.append(_pdistance(seq1, seq2)) #calculate pdistance, append to container
-            start += shift
-            finish = start + window
-
-        distance_data[align[par].id] = dist_container
-        
-    return distance_data
-
-
-def simgen(align, pot_rec, window=500, shift=100, region=False, draw=True):
-    """slices the alignment, collects the distance data
-
-    Parameters:
-    -----------
-    align: the location of the fasta alignment
-    pot_rec: int
-        the number of the sequence under study, starts with 0,
-        like the 'x' dimension in the numpy array
+        Parameters:
+        -----------
+        pot_rec: int
+            the number of the sequence under study, starts with 0,
+            like the 'x' dimension in the numpy array
         window: int
-        sliding window size
-    shift: int
-        the step window slides downstream the alignment
-    region: a tuple or a list of two integers
-        the region of the alignment to analyze. the start
-        and the end nucleotide positions
-    return_data: bool, optional
-        return the data in pandas DataFrame
-        """
+            sliding window size. 500 by default
+        shift: int
+            the step window slides downstream the alignment. 250 by default
+        region: a tuple or a list of two integers
+            the region of the alignment to analyze. the start
+            and the end nucleotide positions
+       
+            """
 
-    assert window >=1, "wondow can't be a negative or zero"
-    assert shift >= 1, "shift can't be a negative or zero" 
-    
-    if region:
-        assert region[0] < region[1], "the value of the first nucleotide position should be less than the second one"
-        align = AlignIO.read(align, "fasta")
-        align = align[:, region[0] : region[1]]
-        left_border = region[0]   # border for the first tick
-        right_border = region[1]  # if region, 'right_border' is actual position
-    else:
-        align = AlignIO.read(align, "fasta")
-        left_border = 1  # border for the first tick
-        right_border = len(align[0, :])
-    
-    # creating tick labels for the plot
-    ticks = _get_x_labels(left_border, right_border, shift)
-    
-    # calculating pairwise distance
-    distance_data = _move_window(align, window, pot_rec, shift)
-    
-    
-    if draw:
-        _draw_simplot(distance_data, ticks) 
-    else:
-        #data = pd.DataFrame(data=distance_data, index=ticks[1:]) # [1:] to map data to index
-        data = distance_data # test line
-        return data
-    
+        assert window >=1, "wondow can't be a negative or zero"
+        assert shift >= 1, "shift can't be a negative or zero" 
+
+        
+        if region:
+            assert region[0] < region[1], "the value of the first nucleotide position should be less than the second one"
+            
+            collect_sliced = []
+            for rec in self._records:  # access to seq of the SeqRecord obj inside MultipleSeqAlignment
+                sliced_seq = rec.seq[region[0]:region[1]]
+                collect_sliced.append(SeqRecord(sliced_seq, id=rec.id, name=rec.name, description=rec.description))
+
+            self._align = MultipleSeqAlignment(collect_sliced)
         
 
-#simgen("C:\\recan\\data\\empty.fasta", pot_rec=1)
-#simgen("C:\\for_recan_lsdv_1.fasta", pot_rec=2)
+            left_border = region[0]   # border for the first tick
+            right_border = region[1]  # if region, 'right_border' is actual position
+
+        else:
+            collect_sliced = []
+            for rec in self._records:  # access to seq of the SeqRecord obj inside MultipleSeqAlignment
+                sliced_seq = rec.seq[:]
+                collect_sliced.append(SeqRecord(sliced_seq, id=rec.id, name=rec.name, description=rec.description))
+
+            self._align = MultipleSeqAlignment(collect_sliced)
         
+            left_border = 1  # border for the first tick
+            right_border = self.get_alignment_length()
+ 
+        # creating tick labels for the plot
+        self._get_x_labels(left_border, right_border, shift)
+
+        # calculating pairwise distance
+        self._move_window(window, pot_rec, shift)
+        
+        self._draw_simplot()
     
     
     
+    def get_df(self, df=True):
+        """return pandas DataFrame object"""
+        if df:
+            return pd.DataFrame(data=self._distance, index=self._ticks[1:]).T
+        else:
+            return self._distance
     
     
     
-    
-    
-    
-    
-    
-    
-    
+    def get_info(self):
+        """shows information about the alignment, 
+        index which is the row number, sequence name, 
+        and alignment length"""
+        
+        print("index:", "sequence id:", sep="\t")
+        for counter, value in enumerate(self):
+            print(counter, value.id, sep="\t")
+        print("alignment length: ", self.get_alignment_length())
+        #print(self)
+        
+        
+        
