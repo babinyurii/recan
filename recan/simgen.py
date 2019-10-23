5b96731f9de468e123cb4077ff93b7bf881ea7d3
Бабин Юрий Юрьевич <babin@pcr.int>
2019-10-23 17:57:58 +0300

    test added and simgen update

--- recan/simgen.py
+++ recan/simgen.py
@@ -2,33 +2,54 @@
 and to explore recombination events using similarity plots
 """
 
-
-
 from Bio.Align import MultipleSeqAlignment
 from Bio.SeqRecord import SeqRecord
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-import matplotlib.pyplot as plt
+from plotly.offline import init_notebook_mode, iplot
+#import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.graph_objs as go
-init_notebook_mode(connected=True)
+init_notebook_mode()
 
 
 
 class Simgen(MultipleSeqAlignment):
+    """
+    A class used to represent an alignment object
+    ---
+    Methods
+    -------
+    simgen(pot_rec, window=500, shift=250, region=False, dist="pdist")
+        outputs similarity plots using slidign window approach
+    get_data(df=True)
+        returns distance data as a pandas DataFrame object or a dictionary of lists 
+        with key as a sequence id and distances in corresponding lists
+    get_info()
+        outputs information about the alignment, index (which is the row number), 
+        sequence names, and alignment length
+    save_data(path=False, out="csv")
+        saves the data spreadsheet as a csv or excel file
+    
+    """
       
     def __init__(self, path):
-        """initializing Simgen"""
+        """
+        initializing Simgen
+        ---
+        Parameters
+        ----------
+        path: str
+            path to the fasta file containing alignment
+        """
         from Bio import AlignIO
         recs_prepared = (x for x in AlignIO.read(path, "fasta")) # it works without it, but i believe it's formally right
         super(Simgen, self).__init__(recs_prepared)  # you need explicitly call the __init__ of the upperclass
-        
         self._distance = {}  # empty, until call of the simgen function
         self._align = None  # current slice or whole MultipleSeqRecord for plotting
         self._ticks = None  # ticks for plot
         
     
     def _draw_simplot(self):
-        """draws similarity plot"""
+        """draws similarity plot using plotly"""
 
         data = []
         for key in self._distance.keys():
@@ -42,25 +63,23 @@ class Simgen(MultipleSeqAlignment):
                 title="sequence identity"),
             legend=dict(x=-0.1, y=1.5, orientation="h"))
             #legend=dict(x=-0.1, y=1.5))
-
         fig = go.Figure(data=data, layout=layout)
         iplot(fig)
         
-        
+    #TODO
+    # tick label merge
     def _draw_simplot_mpl(self):
         """draws similartiy plot using  matplotlib """
         
         data = list(self._distance.values())
         ticks = self._ticks[1:]
-        labels = list(self._distance.keys())
+        #labels = list(self._distance.keys())
         #print(data, ticks, labels, sep="\n")
-        
         plt.figure(figsize=(15, 8))
         for i in data:
             plt.plot(range(1, len(i) + 1), i)
         plt.xticks(list(range(1, len(ticks))), ticks, rotation='vertical')
         plt.show()
-    
     
     
     def _get_x_labels(self, left_border, right_border, shift):
@@ -73,9 +92,8 @@ class Simgen(MultipleSeqAlignment):
             tick_container.append(tick_container[-1] + shift)
             if tick_container[-1] > right_border:
                 tick_container[-1] = right_border
-
-        self._ticks = tick_container
-     
+        #self._ticks = tick_container
+        return tick_container
     
     def _move_window(self, window, pot_rec, shift, dist):
         """moves window"""
@@ -94,17 +112,18 @@ class Simgen(MultipleSeqAlignment):
                 seq2 = self._align[par, start:finish].seq  # here's a parent's slice
                 
                 if dist == "pdist":
-                    dist_container.append(self._pdistance(seq1, seq2)) #calculate pdistance, append to container
+                    distance = self._pdistance(seq1, seq2)
+                    dist_container.append(distance) #calculate pdistance, append to container
                 elif dist == "k2p":
-                    dist_container.append(self._K2Pdistance(seq1, seq2)) #calculate pdistance, append to container
-
-                
+                    distance = self._K2Pdistance(seq1, seq2)
+                    dist_container.append(distance)
+                    #dist_container.append(self._K2Pdistance(seq1, seq2)) #calculate pdistance, append to container
                 start += shift
                 finish = start + window
-
             distance_data[self._align[par].id] = dist_container
-
-        self._distance = distance_data  # do i really should return? it's better to get access just right
+    
+        #self._distance = distance_data  
+        return distance_data
     
     def _K2Pdistance(self, seq1, seq2):
         """
@@ -115,11 +134,9 @@ class Simgen(MultipleSeqAlignment):
         """
         from math import log, sqrt
         pairs = []
-        
         for x in zip(seq1, seq2):
             if '-' not in x: 
                 pairs.append(x)
-
         ts_count=0
         tv_count=0
         length = len(pairs)
@@ -145,7 +162,7 @@ class Simgen(MultipleSeqAlignment):
    
         
     def _pdistance(self, seq1, seq2):
-        """calculates pairwise distance between two sequences"""
+        """calculates 1 - pairwise distance between two sequences"""
         p = 0
         pairs = []
         for x in zip(seq1, seq2):
@@ -160,46 +177,17 @@ class Simgen(MultipleSeqAlignment):
             dist = float(1 - p / length)  # '1 - p' to take plot 'upside down'
             return dist
         except ZeroDivisionError as e:
-            print(e, ": perhaps your alignment contains only gaps")
-
-            
-            
-    def simgen(self, pot_rec, window=500, shift=250, region=False, dist='pdist',
-               inter=True):
-        """slices the alignment, collects the distance data
-
-        Parameters:
-        -----------
-        pot_rec: int
-            the number of the sequence under study, starts with 0,
-            like the 'x' dimension in the numpy array
-        window: int
-            sliding window size. 500 by default
-        shift: int
-            the step window slides downstream the alignment. 250 by default
-        region: a tuple or a list of two integers
-            the region of the alignment to analyze. the start
-            and the end nucleotide positions
-        dist: 'pdist' or 'k2p' 
-            pairwise or Kimura methods to calculate distance
-       
-            """
-
-        assert window >=1, "wondow can't be a negative or zero"
-        assert shift >= 1, "shift can't be a negative or zero" 
-
+            #print(e, ": perhaps your alignment contains only gaps")
+            pass
+        
+    def _get_collect_sliced_left_right_borders(self, region=False):
         
         if region:
             assert region[0] < region[1], "the value of the first nucleotide position should be less than the second one"
-            
             collect_sliced = []
             for rec in self._records:  # access to seq of the SeqRecord obj inside MultipleSeqAlignment
                 sliced_seq = rec.seq[region[0]:region[1]]
                 collect_sliced.append(SeqRecord(sliced_seq, id=rec.id, name=rec.name, description=rec.description))
-
-            self._align = MultipleSeqAlignment(collect_sliced)
-        
-
             left_border = region[0]   # border for the first tick
             right_border = region[1]  # if region, 'right_border' is actual position
 
@@ -208,28 +196,71 @@ class Simgen(MultipleSeqAlignment):
             for rec in self._records:  # access to seq of the SeqRecord obj inside MultipleSeqAlignment
                 sliced_seq = rec.seq[:]
                 collect_sliced.append(SeqRecord(sliced_seq, id=rec.id, name=rec.name, description=rec.description))
-
-            self._align = MultipleSeqAlignment(collect_sliced)
-        
+            
             left_border = 1  # border for the first tick
             right_border = self.get_alignment_length()
- 
-        # creating tick labels for the plot
-        self._get_x_labels(left_border, right_border, shift)
-
-        # calculating pairwise distance
-        self._move_window(window, pot_rec, shift, dist)
-        
-        if inter:
-            self._draw_simplot()
-        else:
-            self._draw_simplot_mpl()
             
-    
-    
-    
+        return collect_sliced, left_border, right_border
+            
+            
+    def simgen(self, pot_rec, window=500, shift=250, region=False, dist='pdist'):
+        """slices the alignment, collects the distance data, outputs the plot
+
+        Parameters:
+        -----------
+        pot_rec: int
+            the index of the sequence under study. use get_info() to find out.
+            it is the index of the row in the alignment: starts with 0,
+            like the 'x' dimension in the numpy array
+        window: int
+            sliding window size. 500 by default
+        shift: int
+            the step window slides downstream the alignment. 250 by default
+        region: a tuple or a list of two integers or False by default
+            the region of the alignment to analyze: the start
+            and the end nucleotide positions. 
+            by default takes the whole alignment length
+        dist: str
+            'pdist' or 'k2p' 
+            pairwise or Kimura methods to calculate distance
+       
+            """
+
+        assert window >=1, "window  parameter can't be a negative or zero"
+        assert shift >= 1, "shift parameter can't be a negative or zero" 
+
+        collect_sliced, left_border, right_border = self._get_collect_sliced_left_right_borders(region)
+        self._align = MultipleSeqAlignment(collect_sliced)
+        # creating tick labels for the plot
+        
+        #self._get_x_labels(left_border, right_border, shift)
+        self._ticks = self._get_x_labels(left_border, right_border, shift)
+        
+        # calculating pairwise distance
+        
+        #self._move_window(window, pot_rec, shift, dist)
+        self._distance = self._move_window(window, pot_rec, shift, dist)
+        
+        self._draw_simplot()
+        #TODO
+        # inter=True, when tick merge will be solved
+        # add this parameter to choose between plotly and mpl
+        #if inter:
+        #    self._draw_simplot()
+        #else:
+        #    self._draw_simplot_mpl()
+            
+
     def get_data(self, df=True):
-        """return pandas DataFrame object"""
+        """returns distance data
+        Parameters
+        ---------
+        df: bool
+            True: returns pandas DataFrame object
+            False: returns a dictionary where keys are the sequence ids and 
+            values are distance data
+        
+        """
         if df:
             return pd.DataFrame(data=self._distance, index=self._ticks[1:]).T
         else:
@@ -237,25 +268,44 @@ class Simgen(MultipleSeqAlignment):
     
     
     def get_info(self):
-        """shows information about the alignment, 
-        index which is the row number, sequence name, 
-        and alignment length"""
+        """outputs information about the alignment: 
+        index (which is the row number), 
+        sequence names, and alignment lengths"""
         
         print("index:", "sequence id:", sep="\t")
         for counter, value in enumerate(self):
             print(counter, value.id, sep="\t")
         print("alignment length: ", self.get_alignment_length())
         #print(self)
-        
-        
-    def save_data(self, path=False):
-        df = pd.DataFrame(data=self._distance, index=self._ticks[1:]).T
-        if path:
-            df.to_csv(path)
-        else:
-            df.to_csv()
-        
     
         
+    def save_data(self, path=False, out="csv", out_name="distance_data"):
+        """saves the data spreadsheet as a csv or excel file
+        Parameters
+        ---------
+        path: str
+            output destination
+        out: str
+            output file format: "csv" or "excel"
+        out_name: str
+            output file name
+        """
         
+        df = pd.DataFrame(data=self._distance, index=self._ticks[1:]).T
+        if path:
+            if out == "csv":
+                df.to_csv(out_name + ".csv")
+            elif out == "excel":
+                writer = pd.ExcelWriter(out_name + ".xlsx")
+                df.to_excel(path)
+            else:
+                print("invalid output file")
+        else:
+            if out == "csv":
+                df.to_csv(out_name + ".csv")
+            elif out == "excel":
+                writer = pd.ExcelWriter(out_name + ".xlsx")
+                df.to_excel(writer)
+            else:
+                print("invalid output file format")
         
---
+++ test/test.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 22 15:57:30 2019
+
+@author: babin
+"""
+from Bio.Align import MultipleSeqAlignment
+
+import unittest
+from test_results import dist_whole_align_ref, dist_win_250_shift_100_ref, dist_whole_align_def_params_k2p
+import sys
+sys.path.append("..")
+from recan.simgen import Simgen._
+
+
+class DistTestCase(unittest.TestCase):
+    
+    
+    def setUp(self):
+        
+        self.sequences = [("AAAAAGGGGG", "AAAAAGGGGG"),
+                            ("AAAAAAAAAT", "AAAAAAAAAA"),
+                            ("AAAAATTTTT", "AAAAAAAAAA"),
+                            ("ATTTTTTTTT", "AGGGGGGGGGG")]
+    
+        self.pdistances = [1.0, 0.9, 0.5, 0.1 ]
+        
+        self.ids = ["AB048704.1_genotype_C_", "AB033555.1_Ba", "AB010291.1_Bj"]
+        
+        self.default_shift = 250
+        self.default_window = 500
+        self.ticks_whole_align_ref = [1, 251, 501, 751, 1001, 1251, 1501, 1751, 2001, 2251, 2501, 2751, 3001, 3215]
+        self.ticks_region_ref = [250, 500, 700]
+        self.pot_rec = 1
+        self.dist_reg_ref = {'AB048704.1_genotype_C_': [0.9319999999999999, 0.945], 'AB010291.1_Bj': [0.992, 0.975]}
+        
+        
+        # whole alignment slice
+        self.sim_obj = Simgen("./hbv_C_Bj_Ba.fasta")
+        self.collect_sliced, self.left_border, self.right_border = self.sim_obj._get_collect_sliced_left_right_borders(region=False)
+        self.whole_align_obj = MultipleSeqAlignment(self.collect_sliced)
+        self.ticks_whole_align = self.sim_obj._get_x_labels(self.left_border, self.right_border, self.default_shift)
+        self.sim_obj._align = MultipleSeqAlignment(self.collect_sliced) # store alignment in _align attr of the sim_obj, otherwise move window will throw error
+        self.distance_whole_align = self.sim_obj._move_window(self.default_window, self.pot_rec, self.default_shift, "pdist")
+        # changin window and shift params
+        self.dist_whole_align_win_250_shift_100 = self.sim_obj._move_window(window=250, pot_rec=self.pot_rec, shift=100, dist="pdist")
+        #k2p distance with the same params
+        self.distance_whole_align_k2p = self.sim_obj._move_window(self.default_window, self.pot_rec, self.default_shift, "k2p")
+        
+        # region slice
+        self.sim_obj_reg = Simgen("./hbv_C_Bj_Ba.fasta")
+        self.collect_sliced_reg, self.left_border_reg, self.right_border_reg = self.sim_obj_reg._get_collect_sliced_left_right_borders(region=(250, 700))
+        self.sliced_align_obj = MultipleSeqAlignment(self.collect_sliced_reg)
+        self.ticks_region = self.sim_obj_reg._get_x_labels(self.left_border_reg, self.right_border_reg, self.default_shift)
+        self.sim_obj_reg._align = MultipleSeqAlignment(self.collect_sliced_reg)
+        self.dist_reg = self.sim_obj_reg._move_window(self.default_window, self.pot_rec, self.default_shift, "pdist")
+        
+        
+        
+    def test_get_collect_sliced_left_right_borders_whole_alignment_ids(self):
+        for counter, value in enumerate(self.collect_sliced):
+            self.assertEqual(value.id, self.ids[counter])
+            
+    def test_get_collect_sliced_left_right_borders_region_ids(self):
+        for counter, value in enumerate(self.collect_sliced_reg):
+            self.assertEqual(value.id, self.ids[counter])
+            
+   
+    def test_get_collect_sliced_left_right_borders_whole_alignment_len(self):
+        for i in self.collect_sliced:
+            self.assertEqual(len(i.seq), 3215)
+            
+    def test_get_collect_sliced_left_right_borders_region_len(self):
+        for i in self.collect_sliced_reg:
+            self.assertEqual(len(i.seq), 450)
+            
+    
+    def test_get_collect_sliced_left_right_borders_whole_alignment(self):
+        self.assertEqual(self.left_border, 1)
+        self.assertEqual(self.right_border, 3215)
+        
+    
+    def test_get_collect_sliced_left_right_borders_region(self):
+        self.assertEqual(self.left_border_reg, 250)
+        self.assertEqual(self.right_border_reg, 700)
+        
+    def test_get_x_labels_whole_align_ticks(self):
+        for counter, value in enumerate(self.ticks_whole_align):
+            self.assertEqual(value, self.ticks_whole_align_ref[counter])
+            
+    def test_get_x_labels_region_ticks(self):
+        for counter, value in enumerate(self.ticks_region):
+            self.assertEqual(value, self.ticks_region_ref[counter])
+            
+        
+    def test_move_window_whole_align_default_params(self):
+        for key in self.distance_whole_align.keys():
+            for counter, value in enumerate(self.distance_whole_align[key]):
+                self.assertEqual(value, dist_whole_align_ref[key][counter] )
+                
+    def test_move_window_whole_align_win_250_shift_100(self):
+        for key in self.dist_whole_align_win_250_shift_100.keys():
+            for counter, value in enumerate(self.dist_whole_align_win_250_shift_100[key]):
+                self.assertEqual(value, dist_win_250_shift_100_ref[key][counter] )
+    
+    def test_move_window_whole_align_def_params_k2p(self):
+        for key in self.distance_whole_align_k2p.keys():
+            for counter, value in enumerate(self.distance_whole_align_k2p[key]):
+                self.assertEqual(value, dist_whole_align_def_params_k2p[key][counter] )
+                
+        
+    def test_move_window_region(self):
+        for key in self.dist_reg.keys():
+            for counter, value in enumerate(self.dist_reg[key]):
+                self.assertEqual(value, self.dist_reg_ref[key][counter])
+            
+
+    def test_pdistance_method(self):
+        for counter, value in enumerate(self.pdistances):
+            dist = self.sim_obj._pdistance(self.sequences[counter][0], self.sequences[counter][1])
+            self.assertEqual(round(dist, 1), value)
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
+        
