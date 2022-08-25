9450330ca4adef37cde9ead2bda0df0e53dc9726
Yuriy Babin <babin.yurii@gmail.com>
2022-08-25 12:59:15 +0300

    Add files via upload

---
+++ recan/calc_pairwise_distance.py
@@ -0,0 +1,238 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 23 12:26:58 2022
+
+@author: yuriy
+"""
+
+from math import log, sqrt
+
+"""
+U	Uracil (RNA)	U
+W	Weak	A/T
+S	Strong	C/G
+M	Amino	A/C
+K	Keto	G/T
+R	Purine	A/G
+Y	Pyrimidine	C/T
+B	Not A	C/G/T
+D	Not C	A/G/T
+H	Not G	A/C/T
+V	Not T	A/C/G
+N	Any	A/C/G/T
+"""
+DEGENERATE_NUCS = ["U", "W", "S", "M", "K", "R", "Y", "B", "D", "H", "V", "N"]
+
+def estimate_nucleotide_frequencies(seq):
+    
+    seq = seq.replace("-","").upper()
+    
+    A = seq.count("A")
+    C = seq.count("C")
+    G = seq.count("G")
+    T = seq.count("T")
+    
+    length = float(len(seq))
+    
+    return [ x / length for x in [A, C ,G, T] ]
+    
+    
+def p_distance(seq1, seq2):
+    """calculates pairwise distance between two sequences
+    distance = num of different nucleotides / total nucleotides
+    """
+    
+    different_nucs = 0
+    nuc_pairs = []
+    
+    for x in zip(seq1, seq2):
+        if '-' not in x and x[0] not in DEGENERATE_NUCS and x[1] not in DEGENERATE_NUCS: # skip gaps
+            nuc_pairs.append(x)
+            
+    for (nuc_1, nuc_2) in nuc_pairs:
+        if nuc_1 != nuc_2:
+            different_nucs += 1
+    
+    total_nucs = len(nuc_pairs)
+    
+    try:
+        distance = float(different_nucs / total_nucs)
+    except ValueError:
+        print("zero division. please, check your alignment for gaps")
+    
+    return distance
+        
+    
+    
+def jc_distance(seq1, seq2):
+    """ 
+    jc_distance = - b log(1 - p_dist / b)
+    ------------
+    b is a constant. b = 3/4 for nucleotides and 19/20 for proteins.
+    p_dist = pairwise distance, which is uncorrected distance between seq1 and seq2
+    """
+    b = 0.75
+    p_dist = p_distance(seq1,seq2)
+    
+    try: 
+        distance = - b * log(1 - p_dist / b)
+    except ValueError: 
+        print ("Tried to take log of a negative number")
+        
+    return distance
+
+
+       
+def k2p_distance(seq1,seq2):
+    """
+    Kimura 2-Parameter distance 
+    k2p_distance = - 0.5 log( (1 - 2p - q)*sqrt(1 - 2q) )
+    where:
+    p : transition frequency
+    q : transversion frequency
+    """
+    nuc_pairs = []
+
+    # collect nuc pairs without gaps
+    for x in zip(seq1,seq2):
+        if '-' not in x and x[0] not in DEGENERATE_NUCS and x[1] not in DEGENERATE_NUCS: 
+            nuc_pairs.append(x)
+        
+    ts_count = 0
+    tv_count = 0
+    total_nucs = len(nuc_pairs)
+    
+    transitions = [ "AG", "GA", "CT", "TC"]
+    transversions = [ "AC", "CA", "AT", "TA",
+                      "GC", "CG", "GT", "TG" ]
+
+    for (nuc_1, nuc_2) in nuc_pairs:
+        if nuc_1 + nuc_2 in transitions: 
+            ts_count += 1 
+        elif nuc_1 + nuc_2 in transversions: 
+            tv_count += 1
+    
+    ts_freq = float(ts_count) / total_nucs
+    tv_freq = float(tv_count) / total_nucs
+    
+    try: 
+        distance = -0.5 * log((1 - 2 * ts_freq - tv_freq) * sqrt( 1 - 2 * tv_freq ))
+    except ValueError: 
+        print ("Tried to take log of a negative number")
+        
+    return distance 
+  
+
+def tamura_distance(seq1,seq2):
+    """
+    Tamura distance = -C log( 1 - P/C - Q ) - 0.5( 1 - C )log( 1 - 2Q )
+    where:
+    P = transition frequency
+    Q = transversion frequency
+    C = GC1 + GC2 - 2 * GC1 * GC2
+    GC1 = GC-content of sequence 1
+    GC2 = GC-coontent of sequence 2
+    """
+    
+    nuc_pairs = []
+    
+    #collect ungapped pairs
+    for x in zip(seq1,seq2):
+        if '-' not in x and x[0] not in DEGENERATE_NUCS and x[1] not in DEGENERATE_NUCS: 
+            nuc_pairs.append(x)
+        
+    ts_count = 0
+    tv_count = 0
+    total_nucs = len(nuc_pairs)
+    
+    transitions = [ "AG", "GA", "CT", "TC"]
+    transversions = [ "AC", "CA", "AT", "TA",
+                      "GC", "CG", "GT", "TG" ]
+
+    for (nuc_1, nuc_2) in nuc_pairs:
+        if nuc_1 + nuc_2 in transitions: 
+            ts_count += 1 
+        elif nuc_1 + nuc_2 in transversions: 
+            tv_count += 1
+    
+    ts_freq = float(ts_count) / total_nucs # p
+    tv_freq = float(tv_count) / total_nucs # q
+    
+    gc1 = sum(estimate_nucleotide_frequencies(seq1)[1:3])
+    gc2 = sum(estimate_nucleotide_frequencies(seq2)[1:3])
+    
+    c = gc1 + gc2 - 2 * gc1 * gc2
+
+    try: 
+        distance = -c * log( 1 - ts_freq / c - tv_freq) - 0.5 * ( 1 - c ) * log ( 1 - 2 * tv_freq )
+    except ValueError: 
+        print("Tried to take log of a negative number")
+        
+    return distance
+
+
+# TODO tajima nei isn't included still
+def tn_distance(seq1, seq2):
+    """ 
+    Tajima-Nei distance = -b log(1 - p / b)
+    where:
+    b = 0.5 * [ 1 - Sum i from A to T(Gi^2+p^2/h) ]
+    h = Sum i from A to G( Sum j from C to T (Xij^2/2*Gi*Gj))
+    p = p-distance, i.e. uncorrected distance between seq1 and seq2
+    Xij = frequency of pair (i,j) in seq1 and seq2, with gaps removed
+    Gi = frequency of base i over seq1 and seq2 """
+    from math import log
+
+    ns = ['A','C','G','T']
+    G = estimate_nucleotide_frequencies(seq1 + seq2)
+    p = p_distance(seq1,seq2)
+    pairs = []
+    h = 0
+
+    #collect ungapped pairs
+    for x in zip(seq1,seq2):
+        if '-' not in x: pairs.append(x)
+       
+    #pair frequencies are calculated for AC, AG, AT, CG, CT, GT (and reverse order)
+    for i in range(len(ns)-1):
+        for j in range(i+1,len(ns)):
+            if i != j: 
+                paircount = pairs.count( (ns[i], ns[j]) ) + pairs.count( (ns[j], ns[i]) )
+            Xij_sq = (float(paircount)/len(pairs))**2
+            GiGj = G[i]*G[j]
+            h += 0.5*Xij_sq/GiGj  #h value used to calculate b
+    
+    b = 0.5*(1-sum([x**2 for x in G])+p**2/h)
+    try: d = -b * log(1 - p/b)
+    except ValueError: 
+        print ("Tried to take log of a negative number")
+        return None
+    return d    
+
+
+
+def calc_pairwise_distance(seq1, seq2, dist_method):
+    
+    if dist_method == "pdist":
+        distance = p_distance(seq1, seq2)
+        
+    elif dist_method == "jcd":
+        distance = jc_distance(seq1, seq2)
+        
+    elif dist_method == "k2p":
+        distance = k2p_distance(seq1, seq2)
+        
+    elif dist_method == "td":
+        distance = tamura_distance(seq1, seq2)
+    
+    #elif dist_method == "tnd":
+    #    distance = tn_distance(seq1, seq2)
+       
+    
+    return 1 - distance
+
+
+
+
+
+
--- recan/simgen.py
+++ recan/simgen.py
@@ -1,60 +1,37 @@
-""" Simgen realizes the interface to manipulate alignment in Jupyter notebook,
-and to explore recombination events using similarity plots
+# -*- coding: utf-8 -*-
 """
+Created on Mon Aug 22 12:56:47 2022
 
-from Bio.Align import MultipleSeqAlignment
-from Bio.SeqRecord import SeqRecord
-#import matplotlib.pyplot as plt
+@author: yuriy
+"""
+import plotly.graph_objs as go
 import pandas as pd
 from plotly.offline import init_notebook_mode, iplot
-import plotly.graph_objs as go
+from rolling_window import RollingWindowOnAlignment
+from calc_pairwise_distance import calc_pairwise_distance
 
 
-
-class Simgen(MultipleSeqAlignment):
-    """
-    A class used to represent an alignment object
-    ---
-    Methods
-    -------
-    simgen(pot_rec, window=500, shift=250, region=False, dist="pdist")
-        outputs similarity plots using slidign window approach
-    get_data(df=True)
-        returns distance data as a pandas DataFrame object or a dictionary of lists 
-        with key as a sequence id and distances in corresponding lists
-    get_info()
-        outputs information about the alignment, index (which is the row number), 
-        sequence names, and alignment length
-    save_data(path=False, out="csv")
-        saves the data spreadsheet as a csv or excel file
+class Simgen():
     
-    """
-      
-    def __init__(self, path):
-        """
-        initializing Simgen
-        ---
-        Parameters
-        ----------
-        path: str
-            path to the fasta file containing alignment
-        """
-        from Bio import AlignIO
-        recs_prepared = (x for x in AlignIO.read(path, "fasta")) # it works without it, but i believe it's formally right
-        super(Simgen, self).__init__(recs_prepared)  # you need explicitly call the __init__ of the upperclass
-        self._distance = {}  # empty, until call of the simgen function
-        self._align = None  # current slice or whole MultipleSeqRecord for plotting
-        self._ticks = None  # ticks for plot
+    def __init__(self, in_file):
         
-    
-    def _draw_simplot(self):
+        self.alignment_roll_window = RollingWindowOnAlignment(in_file)
+        self.align_sliced = None
+        self.ticks_for_x_axis = []
+        self.distance_data = {}
+        self.pot_rec_index = None
+        self.pot_rec_id = None
+        
+        
+    def _plot_distance_by_plotly(self):
+                
         """draws similarity plot using plotly"""
         init_notebook_mode()
         data = []
-        for key in self._distance.keys():
-            trace = go.Scatter(y=self._distance[key], x=self._ticks, name=key)
+        for key in self.distance_data.keys():
+            trace = go.Scatter(y=self.distance_data[key], x=self.ticks_for_x_axis, name=key)
             data.append(trace)
-
+        
         layout = go.Layout(
             xaxis=dict(
                 title="nucleotide position"),
@@ -62,194 +39,157 @@ class Simgen(MultipleSeqAlignment):
                 title="sequence identity"),
             legend=dict(x=-0.1, y=1.5, orientation="h"))
             #legend=dict(x=-0.1, y=1.5))
+            
         fig = go.Figure(data=data, layout=layout)
         iplot(fig)
+        print("potential recombinant: ", self.pot_rec_id)
         
-    #TODO
-    # tick label merge
-    def _draw_simplot_mpl(self):
-        """draws similartiy plot using  matplotlib """
         
-        data = list(self._distance.values())
-        ticks = self._ticks[1:]
-        #labels = list(self._distance.keys())
-        #print(data, ticks, labels, sep="\n")
-        plt.figure(figsize=(15, 8))
-        for i in data:
-            plt.plot(range(1, len(i) + 1), i)
-        plt.xticks(list(range(1, len(ticks))), ticks, rotation='vertical')
-        plt.show()
+    def _get_ticks_for_x_axis(self):
+        
+        self.ticks_for_x_axis.clear()
+        
+        for start_stop_nuc_index in self.align_sliced.keys():
+            # we take a nucleotide position in the middle of the window
+            # it'll be a point we plot
+            middle_nuc = (start_stop_nuc_index[1] - start_stop_nuc_index[0]) / 2
+            self.ticks_for_x_axis.append(start_stop_nuc_index[0] + middle_nuc)
+            
+    
+    def _get_pot_rec_id(self):
+        
+        for slice in self.align_sliced.values():
+            slice_0 = slice
+            break
+        
+        self.pot_rec_id = slice_0[self.pot_rec_index].id
+        
     
     
-    def _get_x_labels(self, left_border, right_border, shift):
-        """creates tick labels"""
-
-        tick_container = []      
-        tick_container.append(left_border)
-
-        while tick_container[-1] < right_border:
-            tick_container.append(tick_container[-1] + shift)
-            if tick_container[-1] > right_border:
-                tick_container[-1] = right_border
-        #self._ticks = tick_container
-        return tick_container
-    
-    def _move_window(self, window, pot_rec, shift, dist):
-        """moves window"""
-        distance_data = {}
-        parents = list(range(0, len(self._align)))
-        parents.remove(pot_rec)
-        align_length = len(self._align[0, :])        
+    def _prepare_distance_data(self):
         
-        for par in parents:
-            dist_container = []
-            start = 0
-            finish = shift
-
-            while start < align_length:
-                seq1 = self._align[pot_rec, start:finish].seq # here is a potential recombinant sequence slice
-                seq2 = self._align[par, start:finish].seq  # here's a parent's slice
-                
-                if dist == "pdist":
-                    distance = self._pdistance(seq1, seq2)
-                    dist_container.append(distance) #calculate pdistance, append to container
-                elif dist == "k2p":
-                    distance = self._K2Pdistance(seq1, seq2)
-                    dist_container.append(distance)
-                    #dist_container.append(self._K2Pdistance(seq1, seq2)) #calculate pdistance, append to container
-                start += shift
-                finish = start + window
-            distance_data[self._align[par].id] = dist_container
-    
-        #self._distance = distance_data  
-        return distance_data
-    
-    def _K2Pdistance(self, seq1, seq2):
-        """
-        Kimura 2-Parameter distance = -0.5 log( (1 - 2p -q) * sqrt( 1 - 2q ) )
-        where:
-        p = transition frequency
-        q = transversion frequency
-        """
-        from math import log, sqrt
-        pairs = []
-        for x in zip(seq1, seq2):
-            if '-' not in x: 
-                pairs.append(x)
-        ts_count=0
-        tv_count=0
-        length = len(pairs)
-
-        transitions = [ "AG", "GA", "CT", "TC"]
-        transversions = [ "AC", "CA", "AT", "TA",
-                          "GC", "CG", "GT", "TG" ]
-
-        for (x, y) in pairs:
-            if x + y in transitions: 
-                ts_count += 1 
-            elif x + y in transversions: 
-                tv_count += 1
-
-        p = float(ts_count) / length
-        q = float(tv_count) / length
-        try: 
-            d = -0.5 * log( (1 - 2*p - q) * sqrt( 1 - 2*q ) )
-        except ValueError: 
-            print ("Tried to take log of a negative number")
-            return None
-        return 1 - d
-   
+        for slice in self.align_sliced.values():
+            slice_0 = slice
+            break
         
-    def _pdistance(self, seq1, seq2):
-        """calculates 1 - pairwise distance between two sequences"""
-        p = 0
-        pairs = []
-        for x in zip(seq1, seq2):
-            if '-' not in x:
-                pairs.append(x)
-        for (x, y) in pairs:
-            if x != y:
-                p += 1
-        length = len(pairs)
-        #assert length > 0, "AssertionError: perhaps your alignment contains only or too many gaps"
-        try:
-            dist = float(1 - p / length)  # '1 - p' to take plot 'upside down'
-            return dist
-        except ZeroDivisionError as e:
-            #print(e, ": perhaps your alignment contains only gaps")
-            pass
+        for seq in slice_0:
+            if seq.id == self.pot_rec_id:
+                continue
+            self.distance_data[seq.id] = []
+            
+    
+    def _calculate_distance(self):
         
-    def _get_collect_sliced_left_right_borders(self, region=False):
+        #print(self.align_sliced)
+        
+        for window_borders, alignment_slice in self.align_sliced.items():
+            for seq in alignment_slice:
+                if seq.id == self.pot_rec_id:
+                    continue
+            
+                distance = self._pdistance(alignment_slice[self.pot_rec_index], seq.seq)
+                self.distance_data[seq.id].append(distance)
+        
+            
+        
+    
+    
+    def simgen(self, pot_rec, window, shift, region=False, dist="pdist"):
+        
         
         if region:
-            assert region[0] < region[1], "the value of the first nucleotide position should be less than the second one"
-            collect_sliced = []
-            for rec in self._records:  # access to seq of the SeqRecord obj inside MultipleSeqAlignment
-                sliced_seq = rec.seq[region[0]:region[1]]
-                collect_sliced.append(SeqRecord(sliced_seq, id=rec.id, name=rec.name, description=rec.description))
-            left_border = region[0]   # border for the first tick
-            right_border = region[1]  # if region, 'right_border' is actual position
-
+            self.align_sliced = self.alignment_roll_window.roll_window_along_alignment_region(window_len=window, 
+                                                                          window_step=shift,
+                                                                          region=region)
         else:
-            collect_sliced = []
-            for rec in self._records:  # access to seq of the SeqRecord obj inside MultipleSeqAlignment
-                sliced_seq = rec.seq[:]
-                collect_sliced.append(SeqRecord(sliced_seq, id=rec.id, name=rec.name, description=rec.description))
-            
-            left_border = 1  # border for the first tick
-            right_border = self.get_alignment_length()
-            
-        return collect_sliced, left_border, right_border
-            
-            
-    def simgen(self, pot_rec, window=500, shift=250, region=False, dist='pdist'):
-        """slices the alignment, collects the distance data, outputs the plot
-
-        Parameters:
-        -----------
-        pot_rec: int
-            the index of the sequence under study. use get_info() to find out.
-            it is the index of the row in the alignment: starts with 0,
-            like the 'x' dimension in the numpy array
-        window: int
-            sliding window size. 500 by default
-        shift: int
-            the step window slides downstream the alignment. 250 by default
-        region: a tuple or a list of two integers or False by default
-            the region of the alignment to analyze: the start
-            and the end nucleotide positions. 
-            by default takes the whole alignment length
-        dist: str
-            'pdist' or 'k2p' 
-            pairwise or Kimura methods to calculate distance
+            self.align_sliced = self.alignment_roll_window.roll_window_along_alignment(window_len=window, 
+                                                                          window_step=shift)
+        self.pot_rec_index = pot_rec
+        self._get_pot_rec_id()
+        self._get_ticks_for_x_axis()
+        self._prepare_distance_data()
+        
+        
+        #for key, value in self.align_sliced.items():
+        #    print(key, value)
+        
+        #for i in self.ticks_for_x_axis:
+        #    print(i)
        
-            """
+        
+        
+        for window_borders, alignment_slice in self.align_sliced.items():
+            for seq in alignment_slice:
+                if seq.id == self.pot_rec_id:
+                    continue
+                seq1 = alignment_slice[self.pot_rec_index].seq
+                seq2 = seq.seq
+                distance = calc_pairwise_distance(seq1=seq1, seq2=seq2, 
+                                                 dist_method=dist) 
+                self.distance_data[seq.id].append(distance)
 
-        assert window >=1, "window  parameter can't be a negative or zero"
-        assert shift >= 1, "shift parameter can't be a negative or zero" 
-
-        collect_sliced, left_border, right_border = self._get_collect_sliced_left_right_borders(region)
-        self._align = MultipleSeqAlignment(collect_sliced)
-        # creating tick labels for the plot
         
-        #self._get_x_labels(left_border, right_border, shift)
-        self._ticks = self._get_x_labels(left_border, right_border, shift)
+        self._plot_distance_by_plotly()
         
-        # calculating pairwise distance
         
-        #self._move_window(window, pot_rec, shift, dist)
-        self._distance = self._move_window(window, pot_rec, shift, dist)
         
-        self._draw_simplot()
-        #TODO
-        # inter=True, when tick merge will be solved
-        # add this parameter to choose between plotly and mpl
-        #if inter:
-        #    self._draw_simplot()
-        #else:
-        #    self._draw_simplot_mpl()
-            
-
+        
+    def save_data(self, path=False, out="csv", out_name="distance_data",
+                  data_cols="plot_ticks"):
+        """saves the data spreadsheet as a csv file
+        Parameters
+        ---------
+        path: str
+            output destination
+        out: str
+            output file format: "csv" or "excel"
+        out_name: str
+            output file name
+        columns: str
+            denoting columns in the spreadsheet: "plot_ticks" by default,
+            or "window_pos" for sliding window start and end nucleotide 
+            position in alignment
+        """
+        
+        if data_cols == "plot_ticks":
+            columns = self.ticks_for_x_axis
+        elif data_cols == "window_pos":
+            columns = [x for x in self.align_sliced.keys()]
+       
+        
+        df = pd.DataFrame.from_dict(self.distance_data, 
+                                    orient='index',
+                                    columns=columns)
+        #print(df)
+        #print(self.ticks_for_x_axis)
+        #df = pd.DataFrame(data=self.distance_data, index=self.ticks_for_x_axis[:]).T
+        if path:
+            if out == "csv":
+                df.to_csv(out_name + ".csv")
+            else:
+                print("invalid output file")
+        else:
+            if out == "csv":
+                df.to_csv(out_name + ".csv")
+            else:
+                print("invalid output file format")
+        
+        #for key in self.align_sliced.keys():
+        #    print(key)
+        
+        #print(self.ticks_for_x_axis)
+        #print(self.align_sliced.keys())
+        #data_to_zip = None
+        #for key, value in self.distance_data.items():
+        #    data_to_zip = value
+        #    break
+        
+        #list_to_zip = list(zip(self.ticks_for_x_axis, data_to_zip))
+        
+        #for i in list_to_zip:
+        #    print(i)
+        
+        
     def get_data(self, df=True):
         """returns distance data
         Parameters
@@ -261,50 +201,30 @@ class Simgen(MultipleSeqAlignment):
         
         """
         if df:
-            return pd.DataFrame(data=self._distance, index=self._ticks[1:]).T
+            return pd.DataFrame(data=self.distance_data, index=self.ticks_for_x_axis).T
         else:
-            return self._ticks[1:], self._distance
-    
-    
+            return self.ticks_for_x_axis, self.distance_data
+        
+        
     def get_info(self):
         """outputs information about the alignment: 
         index (which is the row number), 
         sequence names, and alignment lengths"""
         
         print("index:", "sequence id:", sep="\t")
-        for counter, value in enumerate(self):
-            print(counter, value.id, sep="\t")
-        print("alignment length: ", self.get_alignment_length())
-        #print(self)
+        for seq_index, seq in enumerate(self.alignment_roll_window.align):
+            print(seq_index, seq.id, sep="\t")
+        print("alignment length: ", self.alignment_roll_window.align.get_alignment_length())
+      
+        
+        
+        
+        
+        
+        
+       
     
-        
-    def save_data(self, path=False, out="csv", out_name="distance_data"):
-        """saves the data spreadsheet as a csv or excel file
-        Parameters
-        ---------
-        path: str
-            output destination
-        out: str
-            output file format: "csv" or "excel"
-        out_name: str
-            output file name
-        """
-        
-        df = pd.DataFrame(data=self._distance, index=self._ticks[1:]).T
-        if path:
-            if out == "csv":
-                df.to_csv(out_name + ".csv")
-            elif out == "excel":
-                writer = pd.ExcelWriter(out_name + ".xlsx")
-                df.to_excel(path)
-            else:
-                print("invalid output file")
-        else:
-            if out == "csv":
-                df.to_csv(out_name + ".csv")
-            elif out == "excel":
-                writer = pd.ExcelWriter(out_name + ".xlsx")
-                df.to_excel(writer)
-            else:
-                print("invalid output file format")
-        
+
+    
+    
+    
