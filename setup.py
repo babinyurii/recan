fabbc328b5fb3a15924a5a5d114ff855470ab03f
Yuriy Babin <babin.yurii@gmail.com>
2022-09-09 13:50:16 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ setup(
   long_description = long_description,  # added to package readme on pypi
   long_description_content_type = "text/markdown",  # added to package readme on pypi
   packages = ['recan'],   
-  version = '0.1.2',      
+  version = '0.1.4',      
   license='MIT',        
   description = 'recan: recombination analysis tool',   
   author = 'Yuriy Babin',                  
   author_email = 'babin.yurii@gmail.com',      
   url = 'https://github.com/babinyurii/recan', 
-  download_url = 'https://github.com/babinyurii/recan/archive/v_0.1.2.tar.gz',
+  download_url = 'https://github.com/babinyurii/recan/archive/refs/tags/v_0.1.4.tar.gz',
   keywords = ['DNA recombination', 'bioinformatics', 'genetic distance'],   
   install_requires=[            
           'pandas',
