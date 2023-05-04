b08cfd6f7f29f8342dc248251592cc6c6163d5b2
yuriy babin <babin.yurii@gmail.com>
2023-05-04 10:53:06 +0300

    Add link for web version in init

--- recan/__init__.py
+++ recan/__init__.py
@@ -1,1 +1,2 @@
-
+print("currently new web version of recan is available at: http://yuriyb.pythonanywhere.com")
+print("guide and repository for recan web version: https://github.com/babinyurii/recan_gui")
